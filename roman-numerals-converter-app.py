c433a181d0eea723a189475e71b3a7aa312db152
hasan inceöz <hinceoz@gmail.com>
2023-09-18 11:04:15 -0700

    last changed

--- roman-numerals-converter-app.py
+++ roman-numerals-converter-app.py
@@ -22,7 +22,7 @@ def main_post():
         number = int(alpha)
         if not 0 < number < 4000:
             return render_template('index.html', developer_name='hasan', not_valid=True)
-        return render_template('result.html', number_decimal = number , number_roman= convert(number), developer_name='osvaldo')
+        return render_template('result.html', number_decimal = number , number_roman= convert(number), developer_name='hasan')
     else:
         return render_template('index.html', developer_name='hasan', not_valid=False)
 
